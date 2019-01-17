91d7277c3dedd6df488ef576b17f0484176eb563
Eduardo Henrique Arnold <e.arnold@warwick.ac.uk>
2019-01-17 17:51:27 +0000

    Tags sorted by alphabetical order

--- src/_services/tags.service.js
+++ src/_services/tags.service.js
@@ -10,6 +10,7 @@ function getTags()
   return axios.get(`${config.apiUrl}/tags?filter={"tags.tagIDs":"measurementTag"}`)
   .then(res => {
      var tags = res.data.docs.map(it => it._id)
+     tags.sort()
      return tags
    })
   .catch(err => {
