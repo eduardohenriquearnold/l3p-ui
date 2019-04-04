b2a5c864ec565bda56f6c91b062e4160cbe71dc7
Eduardo Henrique Arnold <e.arnold@warwick.ac.uk>
2019-04-04 15:57:36 +0100

    Using boostrap and bootstrap-vue from npm

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import { store } from './_store';
 import { router } from './_helpers';
 import App from './app/App';
 
+//Import bootstrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+//Start app
 new Vue({
     el: '#app',
     router,
