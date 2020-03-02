ffed5f79bb072efec9643f36bc61b4e1140a868e
Eduardo Henrique Arnold <e.arnold@warwick.ac.uk>
2020-03-02 10:16:36 +0000

    Changed Navbar, removed dropdown option

--- src/home/Navbar.vue
+++ src/home/Navbar.vue
@@ -7,10 +7,12 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-nav-item to="/">Home</b-nav-item>
-      <b-nav-item-dropdown text="Query" right>
-          <b-dropdown-item to="/queryVehicular">Vehicular</b-dropdown-item>
-          <b-dropdown-item to="/querySubjective">Subjective</b-dropdown-item>
-      </b-nav-item-dropdown>
+      <b-nav-item to="/queryVehicular">Vehicular Query</b-nav-item>
+      <b-nav-item to="/querySubjective">Subjective Query</b-nav-item>
+      <!-- <b-nav-item-dropdown text="Query" right> -->
+          <!-- <b-dropdown-item to="/queryVehicular">Vehicular</b-dropdown-item> -->
+          <!-- <b-dropdown-item to="/querySubjective">Subjective</b-dropdown-item> -->
+      <!-- </b-nav-item-dropdown> -->
       <b-nav-item to="/delete">Delete</b-nav-item>
       <b-nav-item to="/download">Download</b-nav-item>
       <b-nav-item to="/login">Logout</b-nav-item>
